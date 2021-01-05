fab99c64a612d8cc79a7012bc65d4c9882b7cce5
FelixA-GitHub <67863506+FelixA-GitHub@users.noreply.github.com>
2021-01-05 18:10:42 -0500

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO("sqlite:".__DIR__."/database.db");
+    $db = new PDO("sqlite:".__DIR__."/journal.db");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo $e->getMessage();
